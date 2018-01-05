d2519cd23ded84cbd8753bdd9435819da63c6525
Justin Wang <justinjaywang@gmail.com>
2018-01-04 22:50:34 -0800

    add parallax stuff

--- assets/scripts/_source/main.js
+++ assets/scripts/_source/main.js
@@ -4,17 +4,15 @@ $(function(){
   var $body = $('body'),
     $window = $(window),
     $document = $(document),
-    scrollInterval = 10,
-    windowHeight = 0,
-    bodyHeight = 0,
-    prevScrollTop = 0,
+    scrollInterval = 1,
     scrollTop = 0,
-    relativeScrollTop = 0,
-    scrollBottom = 0;
+    $headerImage = $('.header__image'),
+    headerImageHeight = 0;
 
   var init = function() {
     $body.addClass('js');
     initExternalLinks();
+    initHeaderImage();
   };
 
   // external links
@@ -22,6 +20,50 @@ $(function(){
 
   var initExternalLinks = function() {
     $('a[href^="http://"], a[href^="https://"]').attr('target', '_blank');
+  };
+
+  // header image
+  // ------------
+
+  var initHeaderImage = function() {
+    if (!$headerImage.length) return;
+    setInitialValues();
+    bindWindowResize();
+
+    if ($window.scrollIntervalId) {
+      // clear previous scrollIntervalId
+      $window.prevScrollIntervalId = $window.scrollIntervalId;
+      clearInterval($window.prevScrollIntervalId);
+    }
+    $window.scrollIntervalId = setInterval(updatePage, scrollInterval);
+  };
+
+  var setInitialValues = function() {
+    headerImageHeight = $headerImage[0].offsetHeight;
+    updateValues();
+  };
+
+  var updatePage = function() {
+    window.requestAnimationFrame(function() {
+      updateValues();
+      updateElements();
+    });
+  };
+
+  var updateValues = function() {
+    scrollTop = window.pageYOffset;
+  };
+
+  var updateElements = function() {
+    $headerImage.css('background-position', '0 '+ scrollTop/2 + 'px');
+  };
+
+  var bindWindowResize = function() {
+    $window.bind('resize',
+      function() {
+        setInitialValues();
+      }
+    );
   };
 
   // initialize
